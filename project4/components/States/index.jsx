1f518f7e2973e6d4bd8428b63293928f7b011d1c
huangyachao <1612929789@qq.com>
2025-08-13 17:08:02 +0800

    Finish Problem 2: Create a new component - states view

--- project4/components/States/index.jsx
+++ project4/components/States/index.jsx
@@ -11,12 +11,61 @@ class States extends React.Component {
     super(props);
     console.log(
       "window.cs142models.statesModel()",
-      window.cs142models.statesModel()
+      window.cs142models.statesModel(),
     );
+    this.state = {
+      inputValue: "",
+      result: window.cs142models.statesModel(),
+    };
+  }
+
+  handleInput(event) {
+    if (event.target.value === "") {
+      this.setState({
+        inputValue: event.target.value,
+        result: window.cs142models.statesModel(),
+      });
+    } else {
+      const filteredResults = window.cs142models
+        .statesModel()
+        .filter((item) =>
+          item.toLowerCase().includes(event.target.value.toLowerCase()),
+        );
+      if (filteredResults.length === 0) {
+        this.setState({ inputValue: event.target.value, result: undefined });
+      } else {
+        this.setState({
+          inputValue: event.target.value,
+          result: filteredResults,
+        });
+      }
+    }
   }
 
   render() {
-    return <div>Replace this with the code for CS142 Project 4, Problem 2</div>;
+    return (
+      <div>
+        <input
+          className="cs142-states-input"
+          type="text"
+          value={this.state.inputValue}
+          placeholder="输入字符串"
+          onInput={(event) => this.handleInput(event)}
+        ></input>
+        <div className="cs142-states-display">{this.state.inputValue}</div>
+        <div>
+          {this.state.result === undefined ? (
+            <div className="cs142-states-prompt">no match state</div>
+          ) : (
+            <ul className="cs142-states-result">
+              {this.state.result.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
