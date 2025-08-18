2685ec4c83b9602b1ee90b67073495ebb40092bf
huangyachao <1612929789@qq.com>
2025-08-18 15:14:29 +0800

    Init project 7

---
+++ project7/components/UserDetail/index.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Card, Typography, Grid, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./styles.css";
+
+/**
+ * Define UserDetail, a React component of CS142 Project 5.
+ */
+class UserDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}, // 初始没有数据
+    };
+    this._isMounted = false;
+  }
+  componentWillUnmount() {
+    this._isMounted = false; // 卸载前标记，防止 setState
+  }
+
+  GetUserDetail() {
+    const userId = this.props.match.params.userId;
+    axios("/user/" + userId).then((data) => {
+      if (this._isMounted) this.setState({ user: data.data });
+    });
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.GetUserDetail();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (JSON.stringify(prevState.user) !== JSON.stringify(this.state.user)) {
+      this.GetUserDetail();
+    }
+  }
+
+  render() {
+    const user = this.state.user;
+    return (
+      <Card className="user-card">
+        {/* 用户名 */}
+        <Typography className="user-name">
+          {user.first_name} {user.last_name}
+        </Typography>
+
+        {/* 用户ID */}
+        <Typography className="user-id">ID: {user._id}</Typography>
+
+        {/* 位置和职业 */}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} className="user-grid-item">
+            <Typography className="user-field">
+              <strong>Location:</strong> {user.location}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} className="user-grid-item">
+            <Typography className="user-field">
+              <strong>Occupation:</strong> {user.occupation}
+            </Typography>
+          </Grid>
+        </Grid>
+
+        {/* 描述 */}
+        <Typography className="user-description">
+          <strong>Description:</strong> {user.description}
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to={`/photos/${user._id}`}
+          style={{ marginTop: "20px" }}
+        >
+          View Photos
+        </Button>
+      </Card>
+    );
+  }
+}
+
+export default UserDetail;
---
+++ project7/components/UserList/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Divider, List, ListItemButton, ListItemText } from "@mui/material";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./styles.css";
+
+/**
+ * Define UserList, a React component of CS142 Project 5.
+ */
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userList: [], // 初始没有数据
+    };
+  }
+
+  componentDidMount() {
+    axios("/user/list").then((data) => {
+      this.setState({ userList: data.data });
+    });
+  }
+
+  render() {
+    const userList = this.state.userList;
+    return (
+      <div>
+        <List component="nav">
+          {userList.map((user) => {
+            return (
+              <React.Fragment key={user._id}>
+                <ListItemButton component={Link} to={`/users/${user._id}`}>
+                  <ListItemText
+                    primary={user.first_name + " " + user.last_name}
+                  />
+                </ListItemButton>
+                <Divider />
+              </React.Fragment>
+            );
+          })}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default UserList;
